6ad1485032649dc56e9c5b916b447ad655463293
Bhargava Pejakala Kakrannaya <BKakrannaya@walmartlabs.com>
2016-04-11 19:18:58 -0700

    Removing keys

--- config/auth.js
+++ config/auth.js
@@ -1,18 +1,18 @@
 module.exports = {
     facebook: {
-        consumerKey: '941325435965625',
-        consumerSecret: '1e349e903d2ffca5a7551eecb12dbfd9',
+        consumerKey: '',
+        consumerSecret: '',
         callbackUrl: 'http://socialauthenticator.com:8000/auth/facebook/callback'
     },
     'twitter': {
-        'consumerKey': 'mtLjDpZUAguF5yHj4QDm8fngC',
-        'consumerSecret': 'DYJCaSyVCcIVXNMvKtNg84oRzldlVYWXMDcZ6MSf41E07HszAv',
-        'tokenSecret': 'fDCoy8NS8HtFHK5V4ie34Wm1FmTosyEM8KDkVHytDAKc5',
+        'consumerKey': '',
+        'consumerSecret': '',
+        'tokenSecret': '',
         'callbackUrl': 'http://socialauthenticator.com:8000/auth/twitter/callback'
     },
     'google': {
-        'clientID': '638424254462-frulp7nciboqgmmuie6vas0t40od6k61.apps.googleusercontent.com',
-        'clientSecret': 'Bv4n-nS2LPbIW-AsCwitXChF',
+        'clientID': '',
+        'clientSecret': '',
         'callbackUrl': 'http://socialauthenticator.com:8000/auth/google/callback'
     }
 }
